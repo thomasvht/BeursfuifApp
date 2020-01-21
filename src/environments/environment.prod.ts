0be299d910dc89e5bf008ebd0e8ebfd1dd2464d1
Thomas Vanhoutte <thomasvanhoutte@MacBook-Pro-van-Thomas.local>
2020-01-21 22:46:33 +0100

    Updated firebase.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCCamSBudlzBonx0YWgbysARfO7u1mTvNg",
+    authDomain: "beursfuifapp.firebaseapp.com",
+    databaseURL: "https://beursfuifapp.firebaseio.com",
+    projectId: "beursfuifapp",
+    storageBucket: "beursfuifapp.appspot.com",
+    messagingSenderId: "571964407440"
+  }
 };
